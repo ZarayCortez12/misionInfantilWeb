0f762dc23c8129d486709cee74f097e9ab0ce3fd
ZarayCotez12 <mariazaraycc@ufps.edu.co>
2024-02-09 23:19:50 -0500

    Realizando Cambios, adaptando IDE

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     identificacion:{
-        type: Number,
+        type: String,
         require: true,
     },
     nombre: {
