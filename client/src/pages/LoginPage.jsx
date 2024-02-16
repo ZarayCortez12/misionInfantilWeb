adb12531f7173816e5cec0ac2b2d34882d05e58c
ZarayCotez12 <mariazaraycc@ufps.edu.co>
2024-02-16 13:16:53 -0500

    Realizada Pantalla Login

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -2,6 +2,13 @@ import { useForm } from 'react-hook-form'
 import { useAuth } from '../context/AuthContext';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../components/styles/LoginStyle.css'
+import logoOrganizacion from '../assets/logoOrganizacion.png';
+import { FaUser, FaChalkboardTeacher, FaArrowAltCircleLeft } from "react-icons/fa";
+import { TbLock } from "react-icons/tb";
+import { MdAdminPanelSettings, MdRemoveRedEye } from "react-icons/md";
+import { PiStudent } from "react-icons/pi";
+import { Link } from "react-router-dom"
 
 function LoginPage() {
 
@@ -10,55 +17,164 @@ function LoginPage() {
         handleSubmit,
         formState: { errors },
     } = useForm();
+
     const {signin, errors: signinErrors, isAuthenticated } = useAuth();
     const navigate = useNavigate();
-    const onSubmit = handleSubmit(data => {
-       signin(data);
-    })
+    const onSubmit = handleSubmit((data) => {
+        console.log("estos son los datos obtenidos del formulario",data);
+        // Agrega el tipo de usuario al objeto data
+        signin(data);
+    });
+    
 
     useEffect(() => {
         if (isAuthenticated) navigate("/sectores");
     }, [isAuthenticated])
 
     return (
-        <div className='flex h-[calc(100vh-100px)] items-center justify-center'>
-            <div className='bg-zinc-800 max-w-md w-full p-10 rounded-md'>
+        <div className='flex h-[calc(100vh-100px)] items-center justify-center space-x-20'>
+
+            <div className="relative">
                 
+                <div className="bg-yellow-700 h-16 fixed top-0 w-full z-10 flex items-center justify-start right-0 m-0 p-0">
+                    
+                    <Link to='/'><FaArrowAltCircleLeft className="text-white ml-4 cursor-pointer top-0 z-20 w-8 h-8"/></Link>
+                    
+                </div>
+            
+            </div>
+            
+
+            <div className='text-center'>
+
+                <img src={logoOrganizacion} alt="Logo de la organización" className='w-86 h-64 mx-auto'/><br></br>
+
+                <h1 className='text-4xl font-bold text-blue-800 mt-4 mb-4'>¡ Bienvenidos !</h1>
+
+            </div>
+
+
+
+            <div className=' contenedor bg-blue-900 max-w-md w-full p-10 rounded-md items-center justify-center relative'>
+                  
                 {signinErrors.map((error, i) => (
                         <div className="bg-red-500 p-2 text-white text-center" key={i}>
                             {error}
                         </div>
                 ))}
 
-                <h1 className='text-2xl font-bold my-2'>Login</h1>
-                
+                <h1 className='text-2xl font-bold my-2 text-white text-center'>Inicio de Sesión</h1>
+                <br></br>
                 <form onSubmit={onSubmit}>
-                    <label>Correo Electronico:</label>
-                    <input 
-                        type="email" 
-                        {...register("correo", { required: true })}
-                        className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-                        placeholder="Correo Electronico"
-                    ></input>
+
+                    <div className="flex items-center">
+                        <div className="text-white mr-2">
+                            <FaUser className='w-5 h-5' />
+                        </div>
+                        <input 
+                            type="text" 
+                            {...register("correo", { required: true })}
+                            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+                            placeholder="Usuario"
+                        ></input>
+                    </div>
                     {errors.correo && (
                         <p className="text-red-500">Correo is required</p>
                     )}
 
-                    <label>Contraseña:</label>
-                    <input 
-                        type="password" 
-                        {...register("contraseña", { required: true })}
-                        className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-                        placeholder="Contaseña"
-                    ></input>
+                    <div className="flex items-center">
+                        <div className="text-white mr-2">
+                            <TbLock className='w-6 h-6'/>
+                        </div>
+                        <input 
+                            type="password" 
+                            {...register("contraseña", { required: true })}
+                            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+                            placeholder="Contraseña"
+                        ></input>
+                    </div>
                     {errors.contraseña && (
                         <p className="text-red-500">Contraseña is required</p>
                     )}
+                    <br></br>
 
-                    <input type="submit" value="Login"></input>
+                    <fieldset className='flex flex-col items-center'>
+
+                        <h1 className='text-xl font-bold my-2 text-white text-center'>¿Cómo deseas ingresar?</h1><br></br>
+
+                        <div className='flex items-center justify-center space-x-4'>
+
+                            <div className='flex flex-col items-center mb-4'>
+                            <MdAdminPanelSettings className='text-4xl text-white' />
+                            <label htmlFor='administrador' className='text-xs text-white'>ADMINISTRADOR</label>
+                            <input
+                                type="radio"
+                                id="eadministrador"
+                                name="option"
+                                value="ADMINISTRADOR"
+                                {...register('option', {
+                                    required: true,
+                                    minLength: 1,
+                                })}
+                            />                            
+                            </div>
+
+                            <div className='flex flex-col items-center mb-4'>
+                            <FaChalkboardTeacher className='text-4xl text-white' />
+                            <label htmlFor='docente' className='text-xs text-white'>DOCENTE</label>
+                            <input
+                                type="radio"
+                                id="docente"
+                                name="option"
+                                value="DOCENTE"
+                                {...register('option', {
+                                    required: true,
+                                    minLength: 1,
+                                })}
+                            />
+                            </div>
+
+                            <div className='flex flex-col items-center mb-4'>
+                            <PiStudent className='text-4xl text-white' />
+                            <label htmlFor='estudiante' className='text-xs text-white'>ESTUDIANTE</label>
+                            <input
+                                type="radio"
+                                id="estudiante"
+                                name="option"
+                                value="ESTUDIANTE"
+                                {...register('option', {
+                                    required: true,
+                                    minLength: 1,
+                                })}
+                            />
+                            </div>
+
+                        </div>
+                        {errors.option && errors.option.type === 'required' && (
+                            <p className="text-red-500">Selecciona al menos una opción</p>
+                        )}
+                        {errors.option && errors.option.type === 'minLength' && (
+                            <p className="text-red-500">Selecciona al menos una opción</p>
+                        )}
+
+                    </fieldset>
+                    
+                    <br></br>
+
+                    <div className="flex items-center justify-center">
+                        <input 
+                            type="submit" 
+                            value="Ingresar"
+                            className="bg-white text-blue-800 px-4 py-2 rounded-md"
+                        />
+                    </div>
+
 
                 </form>
             </div>
+
+            <div className="inferior bg-yellow-700 h-16 fixed bottom-0 right-0 m-0 p-0"></div>
+
         </div>
     )
 }
