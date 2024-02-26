2ba3d0aaec7451f6c7db4f2aa0563598ba78050e
ggaby11 <ggaby11suarez.gomez@gmail.com>
2024-02-26 04:34:50 -0500

    Modulo Admin | Index | Versiculos que cambian cada dia

--- client/src/pages/admin/IndexAdmin.jsx
+++ client/src/pages/admin/IndexAdmin.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Versiculos from './Versiculos.json'
 import { PlusCircleIcon } from '@heroicons/react/24/solid';
 import { Link } from 'react-router-dom';
 
 function IndexAdmin() {
+    const [versiculo, setVersiculo] = useState(null);
+
+    useEffect(() => {
+        const selectNewVersiculo = () => {
+            const today = new Date().toISOString().slice(0, 10); // Toma la fecha actual (YYYY-MM-DD)
+            const index = today.charCodeAt(today.length - 1) % Versiculos.versiculosDelDia.length; // (último dígito del día de hoy -1) % cantidad de versiculos en el archivo json
+        
+            setVersiculo(Versiculos.versiculosDelDia[index]); // Selecciona un versiculo
+        }
+        
+        selectNewVersiculo();
+        
+        // Cada 24h se cambia el versiculo
+        const timerId = setInterval(() => {
+            selectNewVersiculo();
+          }, 24 * 60 * 60 * 1000);
+      
+          // Reset al temporizador
+          return () => clearInterval(timerId);
+        }, []);
+    
+    if (!versiculo) {
+        return <div>Cargando...</div>;
+      }
+
     return(
         <div className='flex flex-col max-h-screen gap-3 md:flex-row mt-5'>
             {/*Bienvenida*/}
@@ -39,11 +65,9 @@ function IndexAdmin() {
                 VERSICULO DEL DIA
                 </p>
                 <p className="text-white text-2xl font-medium text-center bg-custom-brown2 rounded-lg ml-5 mr-5 mt-5 px-5 py-10 md:ml-5 md:mr-5 md:py-10 md:px-5">
-                    <strong>
-                        Porque tanto amó Dios al mundo que dio a su Hijo unigénito, para que todo el que cree en él no se pierda, sino que tenga vida eterna.
-                    </strong>
+                    <strong>{versiculo.texto}</strong>
                 <p className='mt-5 text-base md:mt-5 md:text-2xl'>
-                    Juan 3:16
+                    {versiculo.cita}
                 </p>
                 </p>
             </div>
