9ee3d6e2f5cf653e9d2c543f44bf1edbd655496b
ZarayCotez12 <mariazaraycc@ufps.edu.co>
2024-07-22 16:33:29 -0500

    Actualizados Cambios

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -61,51 +61,49 @@ function LoginPage() {
         </h1>
       </div>
 
+      <div className="contenedor mt-20  bg-blue-900 max-w-md w-full p-10 rounded-md items-center justify-center relative">
+        {signinErrors.map((error, i) => (
+          <div className="bg-red-500 p-2 text-white text-center" key={i}>
+            {error}
+          </div>
+        ))}
 
-
-            <div className='contenedor bg-blue-900 max-w-md w-full p-10 rounded-md items-center justify-center relative'>
-                  
-                {signinErrors.map((error, i) => (
-                    <div className="bg-red-500 p-2 text-white text-center" key={i}>
-                        {error}
-                    </div>
-                ))}
-
-                <h1 className='text-2xl font-bold my-2 text-white text-center'>Inicio de Sesión</h1>
-                <br></br>
-                <form onSubmit={onSubmit}>
-
-                    <div className="flex items-center">
-                        <div className="text-white mr-2">
-                            <FaUser className='w-5 h-5' />
-                        </div>
-                        <input 
-                            type="text" 
-                            {...register("correo", { required: true })}
-                            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-                            placeholder="Usuario"
-                        ></input>
-                    </div>
-                    {errors.correo && (
-                        <p className="text-red-500">Correo is required</p>
-                    )}
+        <h1 className="text-2xl font-bold my-2 text-white text-center">
+          Inicio de Sesión
+        </h1>
+        <br></br>
+        <form onSubmit={onSubmit}>
+          <div className="flex items-center">
+            <div className="text-white mr-2">
+              <FaUser className="w-5 h-5" />
+            </div>
+            <input
+              type="text"
+              {...register("identificacion", { required: true })}
+              className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+              placeholder="Usuario"
+            ></input>
+          </div>
+          {errors.correo && (
+            <p className="text-red-500">Por favor, ingresa tu identificación</p>
+          )}
 
           <div className="flex items-center">
             <div className="text-white mr-2">
               <TbLock className="w-6 h-6" />
             </div>
 
-                        <input 
-                            type="password" 
-                            {...register("clave", { required: true })}
-                            className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
-                            placeholder="Contraseña"
-                        ></input>
-                    </div>
-                    {errors.clave && (
-                        <p className="text-red-500">Contraseña is required</p>
-                    )}
-                    <br></br>
+            <input
+              type="password"
+              {...register("clave", { required: true })}
+              className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+              placeholder="Contraseña"
+            ></input>
+          </div>
+          {errors.clave && (
+            <p className="text-red-500">Contraseña es requerida</p>
+          )}
+          <br></br>
 
           <fieldset className="flex flex-col items-center">
             <h1 className="text-xl font-bold my-2 text-white text-center">
@@ -150,43 +148,60 @@ function LoginPage() {
                 />
               </div>
 
-                            <div className='flex flex-col items-center mb-4'>
-                            <PiStudent className='text-4xl text-white' />
-                            <label htmlFor='estudiante' className='text-xs text-white'>ESTUDIANTE</label>
-                            <br></br>
-                            <input
-                                type="radio"
-                                id="estudiante"
-                                name="option"
-                                value="ESTUDIANTE"
-                                {...register('option', {
-                                    required: true,
-                                    minLength: 1,
-                                })}
-                            />
-                            </div>
-
-                        </div>
-                        {errors.option && errors.option.type === 'required' && (
-                            <p className="text-red-500">Selecciona al menos una opción</p>
-                        )}
-                        {errors.option && errors.option.type === 'minLength' && (
-                            <p className="text-red-500">Selecciona al menos una opción</p>
-                        )}
-
-                    </fieldset>
-                    
-                    <br></br>
-
-                    <div className="flex items-center justify-center">
-                        <input 
-                            type="submit" 
-                            value="Ingresar"
-                            className="bg-white text-blue-600 px-4 py-2 rounded-md transition duration-300 ease-in-out hover:bg-custom-brown1 hover:text-white"
-                        />
-                    </div>
-                </form>
+              <div className="flex flex-col items-center mb-4">
+                <PiStudent className="text-4xl text-white" />
+                <label htmlFor="estudiante" className="text-xs text-white">
+                  ESTUDIANTE
+                </label>
+                <br></br>
+                <input
+                  type="radio"
+                  id="estudiante"
+                  name="option"
+                  value="ESTUDIANTE"
+                  {...register("option", {
+                    required: true,
+                    minLength: 1,
+                  })}
+                />
+              </div>
             </div>
+            {errors.option && errors.option.type === "required" && (
+              <p className="text-red-500">Selecciona al menos una opción</p>
+            )}
+            {errors.option && errors.option.type === "minLength" && (
+              <p className="text-red-500">Selecciona al menos una opción</p>
+            )}
+          </fieldset>
+
+          <style jsx>{`
+            .smaller-text {
+              font-size: 0.85rem;
+            }
+            .smaller-bold {
+              font-size: 0.85rem;
+              font-weight: bold;
+              transition: color 0.3s;
+            }
+            .smaller-bold:hover {
+              color: #735109;
+            }
+          `}</style>
+
+          <p className="text-lg my-2 text-white text-center">
+            <span className="smaller-text">¿Olvidaste tu contraseña? </span>
+            <span className="smaller-bold">Reestablecela Aquí</span>
+          </p>
+          <br />
+          <div className="flex items-center justify-center">
+            <input
+              type="submit"
+              value="Ingresar"
+              className="bg-white text-blue-600 px-4 py-2 rounded-md transition duration-300 ease-in-out hover:bg-custom-brown1 hover:text-white"
+            />
+          </div>
+        </form>
+      </div>
 
       <div className="inferior bg-yellow-700 h-16 fixed bottom-0 right-0 m-0 p-0"></div>
     </div>
